ae83a4d4174bc6adddbe147d736a42827560f176
kayo-xm <olowoesaolukayode@gmail.com>
2025-06-03 06:27:39 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ForexAnalysisDashboard from "./components/ForexAnalysisDashboard";
+
+function App() {
+  return (
+    <div>
+      <ForexAnalysisDashboard />
+    </div>
+  );
+}
+
+export default App;
